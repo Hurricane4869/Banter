8dd01e287edbede8bdb5fa18dc5fda21a58af441
Yudha Prawira <yudha.prawira161005@gmail.com>
2024-04-11 21:55:06 +0700

    Credit Panel
    Membuat credit panel tapi ternyata ada bug pada gambar karena pixel yang tidak sama satu sama lain

--- scenes/main menu/main_menu.gd
+++ scenes/main menu/main_menu.gd
@@ -1,16 +1,40 @@
 class_name MainMenu
 extends Control
 
-@onready var play = $HBoxContainer/VBoxContainer/Play as Button
-@onready var keluar = $HBoxContainer/VBoxContainer/Keluar as Button
+@onready var menu = $menu
+@onready var play = $menu/VBoxContainer/Play as Button
+@onready var keluar = $menu/VBoxContainer/Keluar as Button
+@onready var kredit = $menu/VBoxContainer/Kredit as Button
+@onready var kredit_panel = $Kredit_Panel
+@onready var back_from_kredit = $Kredit_Panel/BackFromKredit as Button
 @onready var start_level = preload("res://scenes/dummy/testing/testing_interaction.tscn") as PackedScene
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle()
+
+func toggle():
+	visible = !visible
+	get_tree().paused = visible
+
+func show_and_hide(first, second):
+	first.show()
+	second.hide()
 
 func _ready():
 	play.button_down.connect(on_start_pressed)
 	keluar.button_down.connect(on_exit_pressed)
+	kredit.button_down.connect(on_kredit_pressed)
+	back_from_kredit.button_down.connect(_on_BackFromKredit_pressed)
 
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
 
 func on_exit_pressed() -> void:
 	get_tree().quit()
+	
+func on_kredit_pressed() -> void:
+	show_and_hide(kredit_panel, menu)
+
+func _on_BackFromKredit_pressed():
+	show_and_hide(menu, kredit_panel)
