d05c7e1931084333924d0f3f680cfdde989c590d
Muhammad Hasbi Ashshidiq <hasbiashshidiq2910@gt.student.pens.ac.id>
2024-05-30 20:10:16 +0700

    Improve animation, some changes on mechanic
    > Add idle animation
    > Add starting direction option on player player inspector
    > Change questtimer into timer node defaultly
    > Finish creating level 1 design, minus send mechanic implementation

--- scripts/hari_pertama.gd
+++ scripts/hari_pertama.gd
@@ -3,7 +3,7 @@ extends Node2D
 #Mechanics
 @onready var interaction_area: InteractionArea = $Environment/AntarPaket
 @onready var timer = $UI/QuestTimer
-@onready var timer_node = get_node("UI/QuestTimer/Timer")
+@onready var character_ali = $Character_Ali
 
 #UI
 @onready var play_button = $UI/PlayDummy
@@ -11,14 +11,13 @@ extends Node2D
 @onready var pause_text = $UI/Pause/Pause_Text
 @onready var pause_menu = $UI/PauseMenu
 @onready var black_overlay = get_node("UI/PauseMenu/ColorRect")
-
-#Sound
-
 @onready var rating_dummy = get_node("UI/RatingSystem/RatingDummy")
 @onready var rating_system = $UI/RatingSystem
 @onready var handphone = $UI/PlayQuest/Handphone
 @onready var play_quest = $UI/PlayQuest
 @onready var play_text = $UI/PlayQuest/PlayButton/Play_Text
+
+#Sound
 @onready var open_handphone_sound = $Sound/open_handphone_sound
 @onready var bgm_lvl_1 = $Sound/day_1_bgm
 
@@ -32,11 +31,11 @@ func _ready():
 	rating_dummy.visible = false
 	pause_text.visible = false
 	play_text.visible = false
-	timer_node.start()
-	get_tree().paused = true
+	timer.start()
 	handphone.play("Quest_Appear");
 	open_handphone_sound.play()
 	await open_handphone_sound.finished
+	get_tree().paused = true
 	
 func _process(delta):
 	interaction_area.interact = Callable(self, "_antar_paket")
@@ -45,9 +44,9 @@ func _process(delta):
 func _antar_paket():
 	print_debug("Paket Sampai")
 	status_paket = true
-	timer_node.set_paused(true)
+	timer.set_paused(true)
 	
-	time_left = timer_node.get_time_left()
+	time_left = timer.get_time_left()
 	rating_system._process_rating(time_left)
 	
 	get_tree().paused = true
